edf7063f1275d1ebd4cbc284989c3227a1a5c280
Aimal Amiri <meaimal@hotmail.com>
2022-06-13 19:34:48 +0430

    Removed unused redux files, and fixed linter errors

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import logger from 'redux-logger';
+import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
+export default configureStore({
   reducer: {
     counter: counterReducer,
   },
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -17,7 +17,7 @@ root.render(
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
