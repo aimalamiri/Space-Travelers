867b1f26b87b4726273229261f804a9e08992726
Aimal Amiri <meaimal@hotmail.com>
2022-06-14 13:23:16 +0430

    Add the router links and the active router link in the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const planet = require('../images/planet.png');
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="logo-section">
-      <img className="logo" src={planet} alt="space travelers" />
-    </div>
+const Navbar = () => {
+  const location = useLocation();
 
-  </div>
-);
+  const activeLink = (link) => {
+    if (location.pathname === link) {
+      return 'active';
+    }
+    return null;
+  };
+
+  return (
+
+    <div className="navbar">
+      <div className="logo-section">
+        <img className="logo" src={planet} alt="space travelers" />
+      </div>
+      <div className="flex">
+        <Link to="/" className={activeLink('/')}>Home</Link>
+        <Link to="/about" className={activeLink('/about')}>About</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
