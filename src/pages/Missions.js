e6c1687be6dcd90a49e05eb445e0d8710963a1b4
Aimal Amiri <meaimal@hotmail.com>
2022-06-14 20:05:44 +0430

    List missions in the Missions page

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,3 +1,25 @@
-const Missions = () => <h2>Missions</h2>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { missionsListAction } from '../redux/missions/missions';
+import Mission from '../components/Mission';
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(missionsListAction());
+  }, []);
+
+  return (
+    <div>
+      <div>Hello</div>
+      {missions.map((mission) => (
+        <Mission key={mission.mission_id} mission={mission} />
+      ))}
+    </div>
+  );
+};
 
 export default Missions;
