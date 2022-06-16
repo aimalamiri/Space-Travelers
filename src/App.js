ce807851749cd0a810b78e9fedd72299ad8033fe
Aimal Amiri <meaimal@hotmail.com>
2022-06-16 13:36:27 +0430

    Fix the rockets link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 import About from './pages/About';
 import Missions from './pages/Missions';
 import './App.css';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <div className="py-5">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Rockets />} />
           <Route path="/missions" element={<Missions />} />
           <Route path="/about" element={<About />} />
         </Routes>
